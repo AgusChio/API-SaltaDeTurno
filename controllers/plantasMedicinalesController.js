5d92aa18cd575352eb4dbbdec0384430fd67de8b
Agustina Chiodini <agus.chio25790@gmail.com>
2023-12-13 10:59:11 -0300

    fix plantas medicinales

--- controllers/plantasMedicinalesController.js
+++ controllers/plantasMedicinalesController.js
@@ -1,11 +1,13 @@
+import plantasMedicinalesDTO from '../DTO/plantasMedicinalesDTO.js';
 import plantasMedicinalesService from '../services/plantasMedicinalesService.js';
 
-
 const plantasMedicinalesController = {
+
     async getAllPlantasMedicinales(req, res) {
         try {
-            const plantasMedicinales = await plantasMedicinalesService.getAllPlantasMedicinales();
-            return res.status(200).json(plantasMedicinales);
+            const plantasMedicinales = await plantasMedicinalesService.getAll();
+            const plantasMedicinalesDTO = plantasMedicinales.map((plantaMedicinal) => plantasMedicinalesDTO(plantaMedicinal));
+            return res.status(200).json(plantasMedicinalesDTO);
         } catch (error) {
             return res.status(500).json({ error });
         }
@@ -14,7 +16,7 @@ const plantasMedicinalesController = {
     async getPlantaMedicinalById(req, res) {
         try {
             const plantaMedicinal = await plantasMedicinalesService.getById(req.params.id);
-            if (!plantaMedicinal) return res.status(404).json({ message: "PlantaMedicinal not found" });
+            if (!plantaMedicin9al) return res.status(404).json({ message: "PlantaMedicinal not found" });
             const plantaMedicinalDTO = plantasMedicinalesDTO(plantaMedicinal);
             return res.status(200).json(plantaMedicinalDTO);
         } catch (error) {
