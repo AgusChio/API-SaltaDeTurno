2e31c44b42aeb1c24d2b80639a0e670e03cab21f
AgusChio <agus.chio25790@gmail.com>
2023-12-17 12:39:23 -0300

    fix user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true, minlength: 4, maxlength: 20 },
     email: { type: String, required: true, unique: true, minlength: 4, maxlength: 50},
-    password: { type: String, required: true, minlength: 8, maxlength: 20},
+    password: { type: String, required: true, minlength: 8},
     status: { type: Boolean, default: false },
     google: { type: Boolean, default: false},
     apiKey: { type: String, default: '' },
