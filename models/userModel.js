f60d380a22e8c83ce876b6f2a33e6168a368eee8
AgusChio <agus.chio25790@gmail.com>
2023-12-20 09:05:41 -0300

    fix user

--- models/userModel.js
+++ models/userModel.js
@@ -4,14 +4,11 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, minlength: 4, maxlength: 50},
     password: { type: String, required: true, minlength: 8},
-    status: { type: Boolean, default: false },
-    google: { type: Boolean, default: false},
     apiKey: { type: String, default: '' },
     rol: { 
         type: String,
         enum: ['admin', 'user'],
         required: true},
-    modoOscuro: { type: Boolean, default: false },
     terminosYCondiciones: { type: Boolean, default: false, required: true },
 });
 
